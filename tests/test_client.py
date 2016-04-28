912f9b4e114a485130029a0248d38539300ea892
anthony-shaw <anthonyshaw@apache.org>
2016-04-28 18:06:16 -0400

    Added more client tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -28,6 +28,25 @@ class TestClient(unittest.TestCase):
         info = self.client.info()
         self.assertEqual(info['version'], '0.5')
 
+    def test_get_data(self):
+        store = self.client.get_data(DatastoreType.RUNNING, ('devices', 'ex0'))
+        self.assertEqual(store['snmp:agent']['enabled'], True)
+
+    def test_set_data_value(self):
+        test = {'my': 'new'}
+        store = self.client.set_data_value(DatastoreType.RUNNING, ('devices', 'ex0'), test)
+        self.assertTrue(store)
+
+    def test_create_data_value(self):
+        test = {'my': 'new'}
+        store = self.client.create_data_value(DatastoreType.RUNNING, ('devices', 'ex0'), test)
+        self.assertTrue(store)
+
+    def test_delete_path(self):
+        test = {'my': 'new'}
+        store = self.client.delete_path(DatastoreType.RUNNING, ('devices', 'ex0'), test)
+        self.assertTrue(store)
+
     def test_get_datastore(self):
         store = self.client.get_datastore(DatastoreType.RUNNING)
         self.assertEqual(store['operations']['lock'], '/api/running/_lock')
@@ -54,7 +73,8 @@ class MockConnection(object):
         'running-application/vnd.yang.datastore-None': 'api-running-datastore.json',
         'rollbacks-application/vnd.yang.api-None': 'api-rollbacks.json',
         'rollbacks-application/vnd.yang.api-86': 'api-rollbacks-86.txt',
-        'running-application/vnd.yang.data-rollback': 'api-rollback-apply.json'
+        'running-application/vnd.yang.data-rollback': 'api-rollback-apply.json',
+        'running-application/vnd.yang.data-devices/ex0': 'api-running-data.json'
     }
 
     def _path_to(self, file):
@@ -81,6 +101,24 @@ class MockConnection(object):
 
     def post(self, resource_type, media_type, data,
              path=None, params=None):
+        key = '%s-%s-%s' % (resource_type,
+                            media_type,
+                            path)
+        with open(self._path_to(MockConnection.path_to_fixture_mapping[key])) as json_file:
+            json_data = json.load(json_file)
+        return json_data
+
+    def put(self, resource_type, media_type, data,
+            path=None, params=None):
+        key = '%s-%s-%s' % (resource_type,
+                            media_type,
+                            path)
+        with open(self._path_to(MockConnection.path_to_fixture_mapping[key])) as json_file:
+            json_data = json.load(json_file)
+        return json_data
+
+    def delete(self, resource_type, media_type,
+               path=None, params=None):
         key = '%s-%s-%s' % (resource_type,
                             media_type,
                             path)
